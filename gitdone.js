479a0f1fa160ba3345d83e610dcbef236e2e5733
Darren <darren@darrenmothersele.com>
2015-11-08 15:52:07 +0000

    add simple element hiding

--- gitdone.js
+++ gitdone.js
@@ -1,12 +1,28 @@
 'use strict';
 
+
 class Gitdone {
   constructor() {
-
+    this.modifyUI(document.URL);
   }
 
-  modifyUI() {
+  modifyUI(url) {
+    if (Gitdone.isIssueListPage(url)) {
+      $('.header, .pagehead-actions, .sunken-menu').hide();
+    }
 
+    if (Gitdone.isIssuesPage(url)) {
+      $('.header, .pagehead-actions, .sunken-menu').hide();
+      $('.protip').hide();
+    }
+
+    if (Gitdone.isRepoPage(url)) {
+      $('.header, .pagehead-actions').hide();
+      $('.sunken-menu-group li:not([aria-label=Issues])').hide();
+      $('.overall-summary, .repository-lang-stats-graph').hide();
+      $('.file-navigation, .file-wrap').hide();
+      $('.only-with-full-nav').hide();
+    }
   }
 
   addElements() {
@@ -17,3 +33,15 @@ class Gitdone {
 
   }
 }
+
+Gitdone.isIssueListPage = function (url) {
+  return url.match(/.*\/issues\/.*/i);
+}
+Gitdone.isIssuesPage = function (url) {
+  return url.match(/.*\/issues/i);
+}
+Gitdone.isRepoPage = function (url) {
+  return url.match(/.*\/github.com\/[^\/]+\/[^\/]+$/i);
+}
+
+var gitdone = new Gitdone();
