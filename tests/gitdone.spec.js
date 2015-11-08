ef942d3c2b53befc1afb62959e6d55c90222a4f3
Ændrew Rininsland <aendrew@aendrew.com>
2015-11-08 11:13:18 +0000

    Adding Chai and scaffolding test spec.

---
+++ tests/gitdone.spec.js
@@ -0,0 +1,12 @@
+let chai = require('chai');
+let path = require('path');
+chai.should();
+
+let Gitdone = require(path.join(__dirname, '..', 'gitdone'));
+
+describe('Gitdone class', () => {
+  describe('constructor', () => {});
+  describe('modifyUI', () => {});
+  describe('addElements', () => {});
+  describe('renderToolbarUI', () => {});
+});
